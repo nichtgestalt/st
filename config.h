6c72245580bab0c356ddba3af202a123f3cbc5d0
nichtgestalt <1993sanderphilipp@gmail.com>
2021-09-06 18:01:39 +0200

    newer

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mononoki Nerd Font Mono:pixelsize=22:antialias=true:autohint=true";
+static char *font = "hack Nerd Font Mono:pixelsize=22:antialias=true:autohint=true";
 static int borderpx = 3;
 
 /*
